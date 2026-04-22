a0230a2855b109b9e95ea9c154f76bf1829a7a08
Akash Naskar <akash.buban2@gmail.com>
2026-04-22 21:53:54 +0530

    complete data ingestion added and working

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from network-security!")
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
 
+import sys
 
-if __name__ == "__main__":
-    main()
+if __name__=='__main__':
+    try:
+        trainingpipelineconfig = TrainingPipelineConfig()
+        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion= DataIngestion(dataingestionconfig)
+        logging.info("Initiate Data ingestion")
+        dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        print(dataingestionartifact)
+    except Exception as e:
+           raise NetworkSecurityException(e,sys)
