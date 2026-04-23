a0aa41f1518c3c07031cbc305f17b1e53576da9c
Akash Naskar <akash.buban2@gmail.com>
2026-04-24 00:56:01 +0530

    data validation completed and working

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -5,6 +5,7 @@ from networksecurity.constant import training_pipeline
 print(training_pipeline.PIPELINE_NAME)
 print(training_pipeline.ARTIFACT_DIR)
 
+#Training pipeline config is for all the datavalidation, data ingestion, etc
 class TrainingPipelineConfig:
     def __init__(self, timestamp= datetime.now()):
         timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
@@ -34,3 +35,18 @@ class DataIngestionConfig:
 
 #Inititate data ingestion is to firstly read the data from MongoDB, create the feature store, 
 # split the data as train-test and then sdave the data
+
+class DataValidationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
+        self.valid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
+        self.valid_train_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.valid_test_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path: str = os.path.join(
+            self.data_validation_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+        )
