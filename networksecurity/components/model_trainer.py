c505bc3b653c05ac09395027314924e6686032b7
Akash Naskar <akash.buban2@gmail.com>
2026-04-26 23:30:23 +0530

    mlflow commands added

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -21,6 +21,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
+import mlflow
 
 class ModelTrainer:
     def __init__(self, model_trainer_config: ModelTrainerConfig, data_transformation_artifact: DataTransformationArtifact):
@@ -29,6 +30,18 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys )
+        
+    def track_mlflow(self, best_model, classificationmetric):
+        with mlflow.start_run():
+            f1_score= classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision", precision_score)
+            mlflow.log_metric("recall_score", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
+
         
     def train_model(self, X_train, y_train, X_test, y_test):
         models={
@@ -79,9 +92,11 @@ class ModelTrainer:
         classification_train_metric = get_classification_score(y_true=y_train, y_pred=y_train_pred)
         
         #Track the MLFlow
+        self.track_mlflow(best_model, classification_train_metric)
 
         y_test_pred = best_model.predict(X_test)
         classification_test_metric= get_classification_score(y_true= y_test, y_pred= y_test_pred)
+        self.track_mlflow(best_model, classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
 
@@ -98,7 +113,6 @@ class ModelTrainer:
                              )
         logging.info(f"Model trainer artifact: {model_trainer_artifact}")
         return model_trainer_artifact
-    
         
 
 
